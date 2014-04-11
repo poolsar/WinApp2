c4f8a54f6d511bef277c3a3b86fd1f6dccdcfa92
poolsar <grigoriy8706@mail.ru>
2014-04-11 23:38:16 +0800

    321

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(159, 98);
+            this.button1.Location = new System.Drawing.Point(82, 160);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -47,7 +47,7 @@
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "6546546";
             this.ResumeLayout(false);
 
         }
